b5ac4a5fe96fbc73b5a11f43484fadaead00243c
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-03 15:04:51 -0800

    creates get actiom for pieces/show in Pieces/Controller.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,4 +4,9 @@ class PiecesController < ApplicationController
         @pieces = Piece.all
         erb :"/pieces/index"
     end
+
+    get '/pieces/:id' do
+        @piece = Piece.find_by(id: params[:id])
+        erb :"/pieces/show"
+    end
 end
