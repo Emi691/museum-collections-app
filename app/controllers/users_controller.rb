da5355a86ed7fa26ba906a6cbd72df138739b408
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-03 10:47:12 -0800

    creates post action for signup in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     get '/signup' do
         erb :"users/signup"
     end
+
+    post '/signup' do
+        @user = User.new(params)
+        if @user.save && !@params.find_all{|element| element == ""}
+            session[user_id] = @user.id 
+            #redirect to :"/users/#{@user.id}"
+        else
+            redirect to :"/signup"
+        end
+    end
 end
