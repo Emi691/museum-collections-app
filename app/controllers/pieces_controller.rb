9acbc03a7a0b4502357033b4cac4780634c4c14d
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-04 09:23:59 -0800

    creates get action to edit pieces in pieces controller.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -10,12 +10,18 @@ class PiecesController < ApplicationController
     end
 
     post '/pieces' do
-        @piece = Piece.create(params[:piece])
+        piece = Piece.create(params[:piece])
         params[:treatments].each do |treatement|
             treatment = Treatment.create(description: "#{treatment}", start_condition: params[:piece][:condition])
-            treatment.piece = @piece
+            treatment.piece = piece
         end
         @piece.save
+        #binding.pry
+    end
+
+    get '/pieces/:id/edit' do
+        @piece = Piece.find_by(id: params[:id])
+        erb :"/pieces/edit"
     end
 
     get '/pieces/:id' do
