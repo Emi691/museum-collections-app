570531421ae29dee89e443394ba1498000ddbe72
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-04 09:00:29 -0800

    creates pieces/new view with form for creating a new piece.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -5,13 +5,17 @@ class PiecesController < ApplicationController
         erb :"/pieces/index"
     end
 
+    get '/pieces/new' do
+        erb :"pieces/new"
+    end
+
+    post '/pieces' do
+        binding.pry
+    end
+    
     get '/pieces/:id' do
         @piece = Piece.find_by(id: params[:id])
         erb :"/pieces/show"
-    end
-
-    get '/pieces/new' do
-        erb :"pieces/new"
     end
 
 end
