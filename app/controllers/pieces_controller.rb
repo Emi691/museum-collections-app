483da27693a765172a49d5d1537306d2681866c8
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-05 10:06:31 -0800

    adds code to get and patch actions for pieces/:id/edit to ensure users may only edit pieces they have created and must be loggin in to do so.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -11,35 +11,44 @@ class PiecesController < ApplicationController
 
     get '/pieces/new' do
         if logged_in?
-        erb :"pieces/new"
-    else
-        redirect to :"/users/login"
-    end
+            erb :"pieces/new"
+        else
+            redirect to :"/users/login"
+        end
     end
 
     post '/pieces' do
-        piece = Piece.create(params[:piece])
+        @piece = Piece.create(params[:piece])
         params[:treatments].each do |treatement|
             treatment = Treatment.create(description: "#{treatment}", start_condition: params[:piece][:condition])
-            treatment.piece = piece
+            treatment.piece = @piece
         end
         @piece.save
-        #binding.pry
+        redirect to :"/pieces/#{@piece.id}"
     end
 
     get '/pieces/:id/edit' do
         @piece = Piece.find_by(id: params[:id])
-        erb :"/pieces/edit"
+        if logged_in? && @piece.user == current_user
+             erb :"/pieces/edit"
+        else
+            redirect to "/users/#{current_user.id}"
+        end
     end
 
     patch '/pieces/:id' do
         @piece = Piece.find_by(id: params[:id])
-        @piece.update(params[:piece])
-        params[:treatments].each do |treatement|
-            treatment = Treatment.create(description: "#{treatment}", start_condition: params[:piece][:condition])
-            treatment.piece = piece
+        if @piece.user == current_user
+            @piece.update(params[:piece])
+            params[:treatments].each do |treatement|
+                treatment = Treatment.create(description: "#{treatment}", start_condition: params[:piece][:condition])
+                treatment.piece = piece
+            end
+            @piece.save
+            redirect to "/pieces/#{@piece.id}"
+        else
+            redierct to "/users/#{current_user.id}"
         end
-        @piece.save
     end
 
     get '/pieces/:id' do
