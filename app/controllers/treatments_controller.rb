c0c7ad8566bf855b4fab26dada0b00dd429adedd
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-04 16:36:41 -0800

    create mark_as_done action in TreatmentsController

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,2 +1,8 @@
 class TreatmentsController < ApplicationController
+
+    get '/treatments/:id/mark_as_done' do
+        @treatment = Treatment.find_by(id: params[:id])
+        @piece = @treatment.piece
+        @treatment.update(done: "true", end_condition: "#{@piece.condition}")
+    end
 end
